1c212868dcf4df40340fdabc8507d8a040b12dcb
SergDulin <serj.dulin@gmail.com>
2023-08-06 18:54:50 +0300

    Code to sorts files into categories, removes empty folders in the folder path, specified by the user.
    - add import "from pathlib import Path" assign the Path class, which allows you to access paths to files.
    - add CATEGORIES. All files in the specified folder will be divided into categories specified in the dictionary based "CATEGORIES" on their extensions.

---
+++ sort.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+CATEGORIES = {
+    "Audio": [".mp3", ".aiff", ".wav", ".aac", ".flac"],
+    "Documents": [".docx", ".doc", ".txt", ".pdf", ".xls", ".xlsx", ".pptx", ".rtf"],
+    "Video": [".avi", ".mp4", ".mov", ".mkv", ".mpeg"],
+    "Image": [".jpeg", ".png", ".pcd", ".jpg", ".svg", ".tiff", ".raw", ".gif", ".bmp"],
+    "Archive": [".zip", ".7-zip", ".7zip", ".rar", ".gz", ".tar"],
+    "Book": [".fb2", ".mobi"]
+}
