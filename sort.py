d964b28f0b6bcfeb371c30a7bd29edd4ac75bb40
SergDulin <serj.dulin@gmail.com>
2023-08-06 19:03:34 +0300

    - Function move_file(file: Path, root_dir: Path, category: str) -> None: This function moves the given file to the specified category within the root directory.
    - Function get_category(file: Path) -> str:
    This function determines the category of a file based on its extension.
    
    - Function sort_folder(folder_path: str) -> str:
    This function sorts files in the specified folder into categories and deletes empty folders.
    
    - Function delete_empty_folders(path: Path) -> None:
    This function deletes empty folders within the specified directory.

--- sort.py
+++ sort.py
@@ -8,3 +8,42 @@ CATEGORIES = {
     "Archive": [".zip", ".7-zip", ".7zip", ".rar", ".gz", ".tar"],
     "Book": [".fb2", ".mobi"]
 }
+
+def get_category(file: Path) -> str:
+    ext = file.suffix.lower()
+    for cat, exts in CATEGORIES.items():
+        if ext in exts:
+            return cat
+    return "Other"
+	
+def move_file(file: Path, root_dir: Path, category: str) -> None:
+    target_dir = root_dir.joinpath(category)
+    if not target_dir.exists():
+        target_dir.mkdir(parents=True)
+    new_name = target_dir.joinpath(file.name)
+    file.rename(new_name)
+
+def delete_empty_folders(path: Path) -> None:
+    for folder in list(path.glob("**/*"))[::-1]:
+        if folder.is_dir() and not any(folder.iterdir()):
+            is_category_folder = any(cat in CATEGORIES.keys() for cat in folder.name)
+            if not is_category_folder:
+                folder.rmdir()
+
+    if path.name != "" and not any(path.iterdir()):
+        path.rmdir()
+
+def sort_folder(folder_path: str) -> str:
+    path = Path(folder_path)
+    
+    if not path.exists():
+        return f"Folder with path {path} doesn't exist."
+    
+    for item in path.glob("**/*"):
+        if item.is_file():
+            category = get_category(item)
+            move_file(item, path, category)
+
+    delete_empty_folders(path)
+    
+    return "All done"
