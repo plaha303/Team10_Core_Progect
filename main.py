63b793af6c0dcaa5b799cdd5dc6d6dd735f8548b
SergDulin <serj.dulin@gmail.com>
2023-08-09 00:11:19 +0300

    Removed unnecessary

--- main.py
+++ main.py
@@ -143,30 +143,7 @@ def edit_birthday():
         return f"Birthday updated for contact '{name}'."
     return f"No contact '{name}' in address book"
 
-def show_birthday_within_days():
-    try:
-        days = int(input("Enter the number of days to check: "))
-    except ValueError:
-        return "Invalid input. Please enter a valid number of days."
 
-    today = datetime.now()
-    target_date = today + timedelta(days=days)
-    
-    birthday_contacts = []
-    for name, record in address_book.data.items():
-        if record.birthday:
-            birth_date = record.birthday.to_datetime().replace(year=today.year)
-            if birth_date.date() == target_date.date():
-                birthday_contacts.append(record)
-
-    if birthday_contacts:
-        output = f"Contacts with birthdays {days} days from now ({target_date.strftime('%d.%m')}):\n\n"
-        for contact in birthday_contacts:
-            contact_info = f"Name: {contact.name}; Phones: {', '.join(str(phone) for phone in contact.phones)}; Birthday: {contact.birthday};"
-            output += f"{contact_info}\n"
-        return output
-    else:
-        return f"No contacts have birthdays {days} days from now ({target_date.strftime('%d.%m')})."
 
 def show_birthday_within_days():
     try:
@@ -238,7 +215,7 @@ def search_by_phone():
 
 def sort_directory():
     folder_path = input("Enter the folder path to sort: ")
-    result = sort.sort_folder(folder_path)  # виклик функції сортування з модуля sortfolder
+    result = sort.sort_folder(folder_path)  
     return result
 
 
